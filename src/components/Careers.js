5f6b2e369a77e4681c7c34db5bd04e40a30abd50
howardbdev <howarddevennish@gmail.com>
2021-01-31 19:07:56 -0500

    update Careers component

---
+++ src/components/Careers.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import WithFooter from '../wrappers/WithFooter'
+import Navbar from './Navbar'
+import CareerCard from './CareerCard'
+
+import { hash } from "../helpers/functions.js";
+
+import jobOpenings from "../dummyData/jobOpenings"
+
+export default function Careers(props) {
+  return (
+    <WithFooter>
+      <div className="h-12 bg-secondary flex justify-center w-full">
+        <Navbar />
+      </div>
+      <div className="flex justify-center items-center w-full flex-col">
+        <h1 className="text-center text-2xl font-bold m-10">
+          Careers
+        </h1>
+        <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-lx">
+          {jobOpenings.map(job => <CareerCard job={job} key={hash(job.title)} />)}
+        </div>
+      </div>
+    </WithFooter>
+  )
+}
