c41c5f1ed41696f47cf1733580b8c7aa659bce3d
howardbdev <howarddevennish@gmail.com>
2021-01-31 19:10:46 -0500

    add some dummy data

---
+++ src/dummyData/jobOpenings.js
@@ -0,0 +1,26 @@
+//TODO: Replace with real data, of course
+
+const jobOpenings = [
+  {
+    id: 1,
+    title: "Senior Accountant"
+  },
+  {
+    id: 2,
+    title: "Junior Accountant"
+  },
+  {
+    id: 3,
+    title: "Software Engineer"
+  },
+  {
+    id: 4,
+    title: "Junior Developer"
+  },
+  {
+    id: 5,
+    title: "Fullstack Architect"
+  },
+]
+
+export default jobOpenings
