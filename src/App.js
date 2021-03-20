c2bfe6073ec7d9c9a964cb3b5a749a6331fbdfba
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2021-03-20 15:28:43 -0700

    app.js prettier reformat

--- src/App.js
+++ src/App.js
@@ -1,56 +1,71 @@
-import './App.css';
-import React,
-{
-  useEffect, 
-  useReducer,
-  useContext
-} 
-from 'react'
+import "./App.css";
+import React, { useEffect, useReducer, useContext } from "react";
 
-import AuthorizedApp from './AuthorizedApp'
-import UnauthorizedApp from './UnauthorizedApp'
+import AuthorizedApp from "./AuthorizedApp";
+import UnauthorizedApp from "./UnauthorizedApp";
 
-import UserContext from './context/UserContext'
-import UserReducer from './reducers/UserReducer'
-import {
-  POPULATE_USER,
-  LOADING,
-  LOGOUT_USER
-} from './actionTypes'
+import UserContext from "./context/UserContext";
+import UserReducer from "./reducers/UserReducer";
+import { POPULATE_USER, LOADING, LOGOUT_USER } from "./actionTypes";
 
+import LoginManager from "./helpers/LoginManager";
+import ApiManager from "./helpers/ApiManager";
+import Parser from "./helpers/Parser";
 
-
-import LoginManager from './helpers/LoginManager'
-import ApiManager from './helpers/ApiManager';
-import Parser from './helpers/Parser'
-
-import AppDecider from './wrappers/AppDecider'
+import AppDecider from "./wrappers/AppDecider";
 
 export default function App() {
-
-  const user = useContext(UserContext)
-  const [state, dispatch] = useReducer(UserReducer, user)
-
-
+  const user = useContext(UserContext);
+  const [state, dispatch] = useReducer(UserReducer, user);
 
   useEffect(() => {
-
     const checkUserStatus = async () => {
-      const user = await ApiManager.getUser()
-      if(user){
-        const businesses = await ApiManager.getBusinesses()
-        const businessesForContext = businesses.map(b => Parser.parseBusinessForContext(b))
-        dispatch({type: POPULATE_USER, payload: {name: user.name, email: user.email, paymentStatusCurrent: user.payment_status_current, businesses: businessesForContext}}) 
-      }else{
-        LoginManager.clearLocalStorage()
-        dispatch({type: LOGOUT_USER})
+      try {
+        const user = await ApiManager.getUser();
+        if (user) {
+          const businesses = await ApiManager.getBusinesses();
+          const businessesForContext = businesses.map((b) =>
+            Parser.parseBusinessForContext(b)
+          );
+          dispatch({
+            type: POPULATE_USER,
+            payload: {
+              name: user.name,
+              email: user.email,
+              paymentStatusCurrent: user.payment_status_current,
+              businesses: businessesForContext
+            }
+          });
+        } else {
+          if (user) {
+            const businesses = await ApiManager.getBusinesses();
+            const businessesForContext = businesses.map((b) =>
+              Parser.parseBusinessForContext(b)
+            );
+            dispatch({
+              type: POPULATE_USER,
+              payload: {
+                name: user.name,
+                email: user.email,
+                paymentStatusCurrent: user.payment_status_current,
+                businesses: businessesForContext
+              }
+            });
+          } else {
+            LoginManager.clearLocalStorage();
+            dispatch({ type: LOGOUT_USER });
+          }
+        }
+      } catch {
+        LoginManager.clearLocalStorage();
+        dispatch({ type: LOGOUT_USER });
       }
-    }
-  
-    dispatch({type: LOADING})
-    checkUserStatus()
-  }, [])
-  
+    };
+
+    dispatch({ type: LOADING });
+    checkUserStatus();
+  }, []);
+
   // BELOW: Testing to see if this is faster than using wrapper
   // const renderApp = () => {
   //   return !!user.data.name ? <AuthorizedApp /> : <UnauthorizedApp />
@@ -58,12 +73,12 @@ export default function App() {
 
   return (
     // <React.StrictMode>
-      <UserContext.Provider value={{state, dispatch}}>
+    <UserContext.Provider value={{ state, dispatch }}>
       <AppDecider>
-          <UnauthorizedApp />
-          <AuthorizedApp />
-        </AppDecider>
-      </UserContext.Provider>
+        <UnauthorizedApp />
+        <AuthorizedApp />
+      </AppDecider>
+    </UserContext.Provider>
     // </React.StrictMode>
-  )
+  );
 }
