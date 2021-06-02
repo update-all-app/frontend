08ec95cd5cbfd1205d4fbc56336219d2bb0f19ba
micahshute <micah.shute@gmail.com>
2021-06-02 01:00:36 -0700

    adds functionality to create location service

--- src/helpers/ApiManager.js
+++ src/helpers/ApiManager.js
@@ -225,6 +225,27 @@ export default class ApiManager {
     )
   }
 
+  static async connectPageToLocation({
+    providerOauthTokenId,
+    locationId,
+    pageId
+  }){
+    const token = await this.protectedRoute()
+    const body = {
+      location_service: {
+        provider_oauth_token_id: providerOauthTokenId,
+        location_id: locationId,
+        page_id: pageId
+      }
+    }
+
+    return await this.post(
+      `${API_SUFFIX}/location_services`,
+      body,
+      token
+    )
+  }
+
   static async get(path, token = null) {
     return await this.request("GET", path, null, token);
   }
