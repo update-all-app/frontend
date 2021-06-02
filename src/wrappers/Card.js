7a77116b7dfa3a7726bb41c1e7334c78c8d5ad8c
micahshute <micah.shute@gmail.com>
2021-06-02 01:01:39 -0700

    updates styles and subcomponents

--- src/wrappers/Card.js
+++ src/wrappers/Card.js
@@ -5,14 +5,15 @@ export default function Card(props){
 
   const {
     onClick=()=>{},
-    clickable=true
+    clickable=true,
+    classNames="p-10"
   } = props
 
   const clickableClasses = clickable ? 'cursor-pointer hover:bg-secondary transition duration-500' : ''
 
   return(
     <div 
-      className={`${clickableClasses} border-l-2 border-primary inline-block shadow-lg p-10 w-80 overflow-hidden m-8`}
+      className={`${clickableClasses} border-l-2 border-primary inline-block shadow-lg ${classNames} w-80 overflow-hidden m-8`}
       onClick={onClick}
     >   
       {props.children}
