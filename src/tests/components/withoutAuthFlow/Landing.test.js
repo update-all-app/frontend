d4b48935565e8f9a3b200f516cc5e8b07ef9fb00
micahshute <micah.shute@gmail.com>
2021-08-21 19:09:21 -0700

    adds tests

--- src/tests/components/withoutAuthFlow/Landing.test.js
+++ src/tests/components/withoutAuthFlow/Landing.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Landing from '../../../components/without_auth_flow/Landing';
-
-beforeEach(() => {
-    render(<Landing />);
-})
+import { MemoryRouter, Route } from 'react-router-dom';
 
 
 describe('Landing', () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <Landing />    
+                <Route path='/login'>This is the login page</Route>
+                <Route path='/signup'>This is the signup page</Route>
+            </MemoryRouter>
+        );
+    });
+
     describe('the login button', () => {
         it('displays the Login button', () => {
             expect(screen.getByRole('button', {
@@ -15,9 +22,14 @@ describe('Landing', () => {
             })).toBeInTheDocument();
         });
 
-        it('redirects to the correct page after button is pressed', () => {
-
-        })
+        it('redirects to the correct page after button is pressed', async () => {
+            const loginButton = screen.getByRole('button', {
+                name: /log in/i
+            });
+            expect(screen.queryByText('This is the login page')).toBeNull();
+            fireEvent.click(loginButton);
+            expect(screen.getByText('This is the login page')).toBeInTheDocument();
+        });
     });
 
     describe('the sign up button', () => {
@@ -28,7 +40,12 @@ describe('Landing', () => {
         });
 
         it('redirects to the correct page after button is pressed', () => {
-
+            const signupButton = screen.getByRole('button', {
+                name: /sign up/i
+            });
+            expect(screen.queryByText('This is the signup page')).toBeNull();
+            fireEvent.click(signupButton);
+            expect(screen.getByText('This is the signup page')).toBeInTheDocument();
         })
     });
 
